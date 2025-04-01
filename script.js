27d39ee1f507a39a6a01cda8905d342517262b6a
miguelalessio <145574803+miguelalessio@users.noreply.github.com>
2025-04-01 08:22:46 -0300

    01/04

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+     const aumentaFontBotao =document.getElementById('aumentar-fonte');
+    
+     let tamanhoAtualFonte = 1;
+
+    aumentaFontBotao.addEventListener('click', function(){
+        tamanhoAtualFonte+= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+});
