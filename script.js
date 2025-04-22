235b4432afd5e4f870201a7d3b0f8d37e0df2472
miguelalessio <145574803+miguelalessio@users.noreply.github.com>
2025-04-22 08:29:13 -0300

    22/04

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
-
+document.addEventListener('DOMContentLoaded', function(){
+    
     const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
     const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
 
-    botaoDeAcessibilidade.addEventListener('click', function (){
+    botaoDeAcessibilidade.addEventListener('click',function(){
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
     })
 
 
+    
+    const aumentaFonteBotao = document .getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document .getElementById('diminuir-fonte');
+    
+    const alternaConstraste = document.getElementById('alterna-contraste');
 
-const aumentaFontBotao = document.getElementById('aumentar-fonte');
-const diminuiFontBotao = document.getElementById('diminuir-fonte');
+    let tamanhoAtualFonte = 1;
+    
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte+= 0.1; 
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
 
-const alternaContraste = document. getElementById('alterna-contraste')
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1; 
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
 
-let tamanhoAtualFonte = 1;
-
-aumentaFontBotao.addEventListener('click', function () {
-    tamanhoAtualFonte += 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    alternaConstraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste');
+    })
 });
 
-diminuiFontBotao.addEventListener('click', function () {
-    tamanhoAtualFonte -= 0.1;
-    document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-});
-
-  alternaContraste.addEventListener('click', function(){ 
-    document.body.classList.toggle('alto-contrate');
-  })
-});
